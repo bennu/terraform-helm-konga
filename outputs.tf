c15e0bb8ca3e7b36e2b7f65973dfd8bfe21cf22f
Julián Véliz <julianvelizc@outlook.com>
2020-10-08 20:20:00 -0500

    chore: fmt outputs

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,10 @@
 output password {
   description = "Password for default user, valid when LDAP is disable"
-  sensitive = true
-  value     = local.password
+  sensitive   = true
+  value       = local.password
 }
 
 output hostname {
   description = "Konga hostname"
-  value     = local.hostname
+  value       = local.hostname
 }
